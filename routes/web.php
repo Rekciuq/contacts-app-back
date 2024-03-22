b0035a27e62b570221d22f8323396b1932deb65d
Danylo <danilaka76@outlook.com>
2024-03-22 13:09:17 +0200

    Show all contacts api method added.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'api/v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
+  Route::apiResource('contacts', ContactController::class);
+});
