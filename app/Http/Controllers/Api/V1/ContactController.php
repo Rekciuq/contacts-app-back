70da7ca8b252089c82c85a9be322877783d5cda4
Danylo <danilaka76@outlook.com>
2024-03-25 11:44:36 +0200

    Pagination for all contacts was removed.

--- app/Http/Controllers/Api/V1/ContactController.php
+++ app/Http/Controllers/Api/V1/ContactController.php
@@ -16,7 +16,7 @@ class ContactController extends Controller
      */
     public function index()
     {
-        return new ContactCollection(Contact::paginate());
+        return new ContactCollection(Contact::all());
     }
 
     /**
